1609bd156320d9b78d200cd20834313bc0aac546
Hartsa <harrinupponen@gmail.com>
2020-01-02 20:26:34 +0200

    first commit

---
+++ src/main/java/com/example/demo/DemoHartsaApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.domain.Hartsa;
+import com.example.demo.domain.HartsaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoHartsaApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(DemoHartsaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHartsaApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner hartsaDemo(HartsaRepository hRepo) {
+        return (args) -> {
+            log.info("save a couple of strings");
+
+            // Save 3 strings
+            hRepo.save(new Hartsa("Hello!..."));
+            hRepo.save(new Hartsa("I am..."));
+            hRepo.save(new Hartsa("Check out ...."));
+        };
+    }
+}
---
+++ src/main/java/com/example/demo/web/HartsaController.java
@@ -0,0 +1,25 @@
+package com.example.demo.web;
+
+import com.example.demo.domain.Hartsa;
+import com.example.demo.domain.HartsaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class HartsaController {
+
+    @Autowired
+    HartsaRepository hRepo;
+
+    //REST-method to fetch all contents
+    @GetMapping(value = "/content")
+    public @ResponseBody
+    List<Hartsa> hartsaListRest() {
+        return (List<Hartsa>) hRepo.findAll();
+    }
+
+}
