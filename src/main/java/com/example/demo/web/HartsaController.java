7a89d7263e6dc904b2f422ebd0050b0ba18e55da
Hartsa <harrinupponen@gmail.com>
2020-01-04 00:06:15 +0200

    added @CrossOrigin to Controller

--- src/main/java/com/example/demo/web/HartsaController.java
+++ src/main/java/com/example/demo/web/HartsaController.java
@@ -4,11 +4,13 @@ import com.example.demo.domain.Hartsa;
 import com.example.demo.domain.HartsaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@CrossOrigin
 @Controller
 public class HartsaController {
 
